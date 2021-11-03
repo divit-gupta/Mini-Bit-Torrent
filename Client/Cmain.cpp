b26595fa07437591914ff4d53809b4a7395b7f88
Divit Gupta <divitgupta@Divits-MacBook-Air.local>
2021-11-03 19:39:35 +0530

    init

---
+++ Client/Cmain.cpp
@@ -0,0 +1,10 @@
+#include "Cheader.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "This is client.cpp";
+    cout << "Hello world";
+    return 0;
+}
---
+++ Tracker/Tmain.cpp
@@ -0,0 +1,10 @@
+#include "Theader.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "This is TRACKER.cpp";
+    cout << "Hello WOrld!";
+    return 0;
+}
